bdaa50f26642dfa18f375edc6e9c5db2278947a1
adam <adamhc23@gmail.com>
2017-09-29 17:04:26 +0100

    twelfth test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -91,3 +91,13 @@ def find_pet_by_name(pet_shop, name)
 
   return nil
 end
+
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet_shop[:pets].delete(pet)
+    end
+  end
+  return nil
+end
